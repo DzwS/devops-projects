08914cf7bbd0e1d2dd3d579a14a11ca7f30420b9
dzws <zhiwei.deng@quantil.com>
2025-09-02 03:30:42 +0000

    update

--- homelab/install.sh
+++ homelab/install.sh
@@ -1,18 +1,9 @@
 kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.15.2/config/manifests/metallb-native.yaml
-curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
-chmod +x kubectl
-sudo mv kubectl /usr/local/bin/
-kubectl version --client
 
-curl -LO https://github.com/derailed/k9s/releases/latest/download/k9s_Linux_amd64.tar.gz
-tar -xvf k9s_Linux_amd64.tar.gz
-sudo mv k9s /usr/local/bin/
-k9s version
-
-
+kubectl apply -f gitops/infra/metallb-config.yaml
 helm repo add argo https://argoproj.github.io/argo-helm
 helm repo update
-helm install argocd argo/argo-cd --namespace argocd --create-namespace -f values.yaml
+helm install argocd argo/argo-cd --namespace argocd --create-namespace -f terraform/argocd/values.yaml
 
 
 
@@ -28,3 +19,30 @@ helm install external-secrets external-secrets/external-secrets \
   --create-namespace \
   --version 0.15.0 \
   --set installCRDs=true
+
+
+kubectl create ns vault
+helm repo add hashicorp https://helm.releases.hashicorp.com
+helm install vault hashicorp/vault \
+       --set='server.dev.enabled=true' \
+       --set='ui.enabled=true' \
+       --set='ui.serviceType=LoadBalancer' \
+       --namespace vault
+# 默认密码是root
+
+
+# Add the External Secrets Helm repository
+helm repo add external-secrets https://charts.external-secrets.io
+
+# Update Helm repositories
+helm repo update
+
+# Install External Secrets Operator
+helm install external-secrets external-secrets/external-secrets \
+  --namespace external-secrets \
+  --create-namespace \
+  --version 0.15.0
+
+kubectl  apply -f terraform/eso/secretstore.yaml 
+
+kubectl create ns homepage
